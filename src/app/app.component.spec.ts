97431683389f5628b3103a8d43c0501e2afe0a26
ditadev <abrahaminyaka@yahoo.com>
2023-02-23 18:10:44 +0100

    Initial Commit for frontend

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ReelReflection-WebClient'`, () => {
+  it(`should have as title 'ReelReflection.WebClient'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ReelReflection-WebClient');
+    expect(app.title).toEqual('ReelReflection.WebClient');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ReelReflection-WebClient app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('ReelReflection.WebClient app is running!');
   });
 });
