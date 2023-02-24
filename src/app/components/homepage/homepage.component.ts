edac559695ca08d21b179e95949e468bef3b7ea4
ditadev <abrahaminyaka@yahoo.com>
2023-02-24 01:22:25 +0100

    Frontend implementation complete

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -10,64 +10,64 @@ import { ApiService } from 'src/app/services/api.service';
 })
 export class HomepageComponent {
 
-  showMore:boolean=false;
-  toogleHistory:boolean=false;
-  movieById:boolean=false;
-  showHistory:number=0;
+  showMore: boolean = false;
+  toogleHistory: boolean = false;
+  movieById: boolean = false;
+  showHistory: number = 0;
   subscription?: Subscription;
   title: string = '';
-  movie!:Movie;
-  movies!:Movie[];
-  check:string="";
+  movie!: Movie;
+  movies!: Movie[];
+  check: string = "";
 
   constructor(
     private apiService: ApiService,
-  ) {}
+  ) { }
 
 
-  show(){
-    this.showMore=!this.showMore;
+  show() {
+    this.showMore = !this.showMore;
   }
 
   searchMovie(): void {
     this.subscription = this.apiService
-    .searchMovieByTitle(this.title)
-    .subscribe(
-      response => {
-        if (response) { 
-          this.movie = response;
-          console.log("By Search "+response);
-          this.check=this.movie.title;
-        }
-    });
-    this.showHistory=1;
+      .searchMovieByTitle(this.title)
+      .subscribe(
+        response => {
+          if (response) {
+            this.movie = response;
+            console.log("By Search " + response);
+            this.check = this.movie.title;
+          }
+        });
+    this.showHistory = 1;
     this.searchHistory();
   }
 
-  getMovieById(): void{
+  getMovieById(): void {
     this.subscription = this.apiService
-    .getMovieById(this.movie.imdbID)
-    .subscribe(
-      response => {
-        if (response) { 
-          this.movie = response;
-          console.log("By Id "+response);
-        }
-    });
-    this.movieById=!this.movieById;
+      .getMovieById(this.movie.imdbID)
+      .subscribe(
+        response => {
+          if (response) {
+            this.movie = response;
+            console.log("By Id " + response);
+          }
+        });
+    this.movieById = !this.movieById;
   }
 
-  searchHistory(): void{
+  searchHistory(): void {
     this.subscription = this.apiService
-    .getSearchHistory()
-    .subscribe(
-      response => {
-        if (response) { 
-          this.movies = response;
-          console.log("History "+response);
-        }
-    });
-    this.toogleHistory=!this.toogleHistory;
+      .getSearchHistory()
+      .subscribe(
+        response => {
+          if (response) {
+            this.movies = response;
+            console.log("History " + response);
+          }
+        });
+    this.toogleHistory = !this.toogleHistory;
 
   }
 
